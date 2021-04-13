e7eccd007ac09d5369024b53d46744c1498ec532
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-04-12 18:26:15 -0600

    Deprecate old entrypoint

---
+++ AutoCreatePage.php
@@ -0,0 +1,19 @@
+<?php
+
+if ( function_exists( 'wfLoadExtension' ) ) {
+	wfLoadExtension( 'AutoCreatePage' );
+
+	$wgMessagesDirs['AutoCreatePage'] = __DIR__ . '/i18n';
+
+	$wgExtensionMessagesFiles['AutoCreatePageMagic'] = __DIR__ . 'AutoCreatePage.i18n.magic.php';
+
+	wfWarn(
+		'Deprecated PHP entry point used for the AutoCreatePage extension. ' .
+		'Please use wfLoadExtension() instead, ' .
+		'see https://www.mediawiki.org/wiki/Special:MyLanguage/Manual:Extension_registration for more details.'
+	);
+
+	return;
+} else {
+	die( 'This version of the AutoCreatePage extension requires MediaWiki 1.35+' );
+}
