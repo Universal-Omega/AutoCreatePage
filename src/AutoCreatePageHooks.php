b9266c77c1ded3fd0c2b12ec820fd3e5b7f7b978
Universal Omega <54654040+Universal-Omega@users.noreply.github.com>
2021-04-18 01:26:17 -0600

    Update AutoCreatePageHooks.php

--- src/AutoCreatePageHooks.php
+++ src/AutoCreatePageHooks.php
@@ -17,6 +17,41 @@ class AutoCreatePageHooks {
 	}
 
 	/**
+	 * @param WikiPage &$wikiPage
+	 * @param PreparedEdit &$editInfo
+	 * @param bool $changed
+	 */
+	public static function onArticleEditUpdates( WikiPage &$wikiPage, PreparedEdit &$editInfo, bool $changed ) {
+		global $wgAutoCreatePageMaxRecursion;
+
+		$createPageData = $editInfo->output->getExtensionData( 'createPage' );
+		if ( is_null( $createPageData ) ) {
+			return true; // no pages to create
+		}
+
+		// Prevent pages to be created by pages that are created to avoid loops:
+		$wgAutoCreatePageMaxRecursion--;
+
+		$sourceTitle = $wikiPage->getTitle();
+		$sourceTitleText = $sourceTitle->getPrefixedText();
+
+		foreach ( $createPageData as $pageTitleText => $pageContentText ) {
+			$pageTitle = Title::newFromText( $pageTitleText );
+
+			if ( !is_null( $pageTitle ) && !$pageTitle->isKnown() && $pageTitle->canExist() ){
+				$newWikiPage = new WikiPage( $pageTitle );
+				$pageContent = ContentHandler::makeContent( $pageContentText, $sourceTitle );
+				$newWikiPage->doEditContent( $pageContent,
+					"Page created automatically by parser function on page [[$sourceTitleText]]" ); //TODO i18n
+			}
+		}
+
+		// Reset state. Probably not needed since parsing is usually done here anyway:
+		$editInfo->output->setExtensionData( 'createPage', null ); 
+		$wgAutoCreatePageMaxRecursion++;
+	}
+
+	/**
 	 * @param Parser $parser
 	 */
 	public static function onParserFirstCallInit( Parser $parser ) {
@@ -67,42 +102,6 @@ class AutoCreatePageHooks {
 		$parser->getOutput()->setExtensionData( 'createPage', $createPageData );
 
 		return '';
-	}
-
-	
-	/**
-	 * @param WikiPage &$wikiPage
-	 * @param PreparedEdit &$editInfo
-	 * @param bool $changed
-	 */
-	public static function onArticleEditUpdates( WikiPage &$wikiPage, PreparedEdit &$editInfo, bool $changed ) {
-		global $wgAutoCreatePageMaxRecursion;
-
-		$createPageData = $editInfo->output->getExtensionData( 'createPage' );
-		if ( is_null( $createPageData ) ) {
-			return true; // no pages to create
-		}
-
-		// Prevent pages to be created by pages that are created to avoid loops:
-		$wgAutoCreatePageMaxRecursion--;
-
-		$sourceTitle = $wikiPage->getTitle();
-		$sourceTitleText = $sourceTitle->getPrefixedText();
-
-		foreach ( $createPageData as $pageTitleText => $pageContentText ) {
-			$pageTitle = Title::newFromText( $pageTitleText );
-
-			if ( !is_null( $pageTitle ) && !$pageTitle->isKnown() && $pageTitle->canExist() ){
-				$newWikiPage = new WikiPage( $pageTitle );
-				$pageContent = ContentHandler::makeContent( $pageContentText, $sourceTitle );
-				$newWikiPage->doEditContent( $pageContent,
-					"Page created automatically by parser function on page [[$sourceTitleText]]" ); //TODO i18n
-			}
-		}
-
-		// Reset state. Probably not needed since parsing is usually done here anyway:
-		$editInfo->output->setExtensionData( 'createPage', null ); 
-		$wgAutoCreatePageMaxRecursion++;
 	}
 }
 
